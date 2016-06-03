84a766f8700ba5558ebb274101badd66fc0c3dc7
Ole Lensmar <ole.lensmar@smartbear.com>
2016-06-03 09:28:31 -0400

    updated version and infoUrl in PluginInfo

--- src/main/groovy/com/smartbear/swagger/PluginConfig.java
+++ src/main/groovy/com/smartbear/swagger/PluginConfig.java
@@ -7,9 +7,9 @@ import com.eviware.soapui.plugins.PluginConfiguration;
  * Created by ole on 08/06/14.
  */
 
-@PluginConfiguration(groupId = "com.smartbear.soapui.plugins", name = "Swagger Plugin", version = "2.3.1",
+@PluginConfiguration(groupId = "com.smartbear.soapui.plugins", name = "Swagger Plugin", version = "2.4.0-SNAPSHOT",
         autoDetect = true, description = "Provides Swagger 1.X/2.0 import/export functionality for REST APIs",
-        infoUrl = "https://github.com/olensmar/soapui-swagger-plugin")
+        infoUrl = "https://github.com/SmartBear/readyapi-swagger-plugin")
 public class PluginConfig extends PluginAdapter {
     @Override
     public void initialize() {
