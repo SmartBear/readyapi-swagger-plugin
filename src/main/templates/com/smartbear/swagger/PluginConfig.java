5fbcafcf939c1a468d5a919034f7c5cd06fa506b
Ole Lensmar <ole.lensmar@smartbear.com>
2016-08-30 15:21:34 +0200

    fixed build to generate PluginConfig from pom properties

---
+++ src/main/templates/com/smartbear/swagger/PluginConfig.java
@@ -0,0 +1,30 @@
+/**
+ *  Copyright 2016 SmartBear Software, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.smartbear.swagger;
+
+import com.eviware.soapui.plugins.PluginAdapter;
+import com.eviware.soapui.plugins.PluginConfiguration;
+import com.eviware.soapui.support.UISupport;
+
+@PluginConfiguration(groupId = "${project.groupId}",
+    name = "${project.name}",
+    version = "${project.version}",
+    autoDetect = true,
+    description = "${project.description}",
+    infoUrl = "${project.url}" )
+public class PluginConfig extends PluginAdapter {
+}
